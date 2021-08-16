9b7e1eeae56d8f3b662e6b1ad0400cfbb4d04ada
PollyPlayz <74987674+PollyPlayz@users.noreply.github.com>
2021-08-16 16:35:38 +0100

    Added EVEN more wood type jsons for future and existing wood types. Fixed some errors I had made in some

--- src/main/java/com/kangalia/projectdinosaur/core/init/ItemInit.java
+++ src/main/java/com/kangalia/projectdinosaur/core/init/ItemInit.java
@@ -2,7 +2,9 @@ package com.kangalia.projectdinosaur.core.init;
 
 import com.kangalia.projectdinosaur.ProjectDinosaur;
 import com.kangalia.projectdinosaur.core.itemgroup.DinoItems;
+import net.minecraft.item.BoatItem;
 import net.minecraft.item.Item;
+import net.minecraft.item.SignItem;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -13,7 +15,5 @@ public class ItemInit {
     public static final RegistryObject<Item> IRON_CHISEL = ITEMS.register("iron_chisel", () -> new Item(new Item.Properties().durability(250).tab(DinoItems.DINO_ITEMS)));
     public static final RegistryObject<Item> DIAMOND_CHISEL = ITEMS.register("diamond_chisel", () -> new Item(new Item.Properties().durability(1561).tab(DinoItems.DINO_ITEMS)));
     public static final RegistryObject<Item> NETHERITE_CHISEL = ITEMS.register("netherite_chisel", () -> new Item(new Item.Properties().durability(2031).tab(DinoItems.DINO_ITEMS)));
-    public static final RegistryObject<Item> PETRIFIED_BOAT = ITEMS.register("petrified_boat", () -> new Item(new Item.Properties().tab(DinoItems.DINO_ITEMS)));
-    public static final RegistryObject<Item> PETRIFIED_SIGN = ITEMS.register("petrified_sign", () -> new Item(new Item.Properties().tab(DinoItems.DINO_ITEMS)));
 
 }
