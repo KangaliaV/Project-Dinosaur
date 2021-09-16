58c9124f6159c497422b43f43890bfdcb24cff5d
KangaliaV <mia.bellclan@gmail.com>
2021-09-16 09:59:23 +0100

    Basic formatting and code optimisation changes.

--- src/main/java/com/kangalia/projectdinosaur/core/init/BlockInit.java
+++ src/main/java/com/kangalia/projectdinosaur/core/init/BlockInit.java
@@ -2,16 +2,23 @@ package com.kangalia.projectdinosaur.core.init;
 
 import com.kangalia.projectdinosaur.ProjectDinosaur;
 import com.kangalia.projectdinosaur.common.block.FossilExcavator;
+import com.kangalia.projectdinosaur.core.itemgroup.DinoBlocks;
 import net.minecraft.block.*;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.material.MaterialColor;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
 import net.minecraftforge.common.ToolType;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
+import java.util.function.Supplier;
+
 public class BlockInit {
     public static DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, ProjectDinosaur.MODID);
+
+    //Blocks
 
     //Petrified Wood
     public static final RegistryObject<Block> PETRIFIED_LOG = BLOCKS.register("petrified_log", () -> new RotatedPillarBlock(AbstractBlock.Properties.of(Material.WOOD, MaterialColor.COLOR_BROWN)
@@ -79,4 +86,17 @@ public class BlockInit {
 
     //Machine Blocks
     public static final RegistryObject<Block> FOSSIL_EXCAVATOR = BLOCKS.register("fossil_excavator", () -> new FossilExcavator());
+
+
+    //Helper Methods
+
+    private static <T extends Block>RegistryObject<T> registerBlock(String name, Supplier<T> block) {
+        RegistryObject<T> toReturn = BLOCKS.register(name, block);
+        registerBlockItem(name, toReturn);
+        return toReturn;
+    }
+
+    private static <T extends Block> void registerBlockItem(String name, RegistryObject<T> block) {
+        ItemInit.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties().tab(DinoBlocks.DINO_BLOCKS)));
+    }
 }
--- src/main/java/com/kangalia/projectdinosaur/core/init/ItemInit.java
+++ src/main/java/com/kangalia/projectdinosaur/core/init/ItemInit.java
@@ -10,6 +10,7 @@ import net.minecraftforge.registries.ForgeRegistries;
 public class ItemInit {
     public static DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, ProjectDinosaur.MODID);
 
+    //Chisels
     public static final RegistryObject<Item> IRON_CHISEL = ITEMS.register("iron_chisel", () -> new Item(new Item.Properties().durability(250).tab(DinoItems.DINO_ITEMS)));
     public static final RegistryObject<Item> DIAMOND_CHISEL = ITEMS.register("diamond_chisel", () -> new Item(new Item.Properties().durability(1561).tab(DinoItems.DINO_ITEMS)));
     public static final RegistryObject<Item> NETHERITE_CHISEL = ITEMS.register("netherite_chisel", () -> new Item(new Item.Properties().durability(2031).tab(DinoItems.DINO_ITEMS)));
