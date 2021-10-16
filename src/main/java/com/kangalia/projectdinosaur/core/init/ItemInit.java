ba67095593a2561e1fdc8912998e6be71eb2b7bb
mia <mia.bellclan@gmail.com>
2021-10-16 17:59:29 +0100

    Added full functionality for the Plaster item, including the advancement, recipes, and usage (plastering fossil blocks).

--- src/main/java/com/kangalia/projectdinosaur/core/init/ItemInit.java
+++ src/main/java/com/kangalia/projectdinosaur/core/init/ItemInit.java
@@ -1,6 +1,7 @@
 package com.kangalia.projectdinosaur.core.init;
 
 import com.kangalia.projectdinosaur.ProjectDinosaur;
+import com.kangalia.projectdinosaur.common.item.Plaster;
 import com.kangalia.projectdinosaur.core.itemgroup.DinoItems;
 import net.minecraft.item.BoatItem;
 import net.minecraft.item.Item;
@@ -16,5 +17,9 @@ public class ItemInit {
     public static final RegistryObject<Item> IRON_CHISEL = ITEMS.register("iron_chisel", () -> new Item(new Item.Properties().durability(250).tab(DinoItems.DINO_ITEMS)));
     public static final RegistryObject<Item> DIAMOND_CHISEL = ITEMS.register("diamond_chisel", () -> new Item(new Item.Properties().durability(1561).tab(DinoItems.DINO_ITEMS)));
     public static final RegistryObject<Item> NETHERITE_CHISEL = ITEMS.register("netherite_chisel", () -> new Item(new Item.Properties().durability(2031).tab(DinoItems.DINO_ITEMS)));
+
+    //Plaster
+    public static final RegistryObject<Item> PLASTER = ITEMS.register("plaster", () -> new Plaster(new Item.Properties().tab(DinoItems.DINO_ITEMS)));
+
 
 }
