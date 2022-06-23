0a49f874f19734912566ad9bea42ed1cce8b2911
mia <mia.bellclan@gmail.com>
2022-06-23 20:46:12 +0100

    Fixed Dino Scanner GUI and added relevant translatable labels.

--- src/main/java/com/kangalia/projectdinosaur/client/gui/DinoScannerScreen.java
+++ src/main/java/com/kangalia/projectdinosaur/client/gui/DinoScannerScreen.java
@@ -2,18 +2,13 @@ package com.kangalia.projectdinosaur.client.gui;
 
 import com.kangalia.projectdinosaur.ProjectDinosaur;
 import com.kangalia.projectdinosaur.common.container.DinoScannerContainer;
-import com.kangalia.projectdinosaur.common.item.DinoScanner;
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
-import net.minecraft.client.gui.chat.NarratorChatListener;
-import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.player.Inventory;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ItemStack;
 
 import javax.annotation.Nonnull;
 
@@ -22,8 +17,9 @@ public class DinoScannerScreen extends AbstractContainerScreen<DinoScannerContai
     private final ResourceLocation SCANNER = new ResourceLocation(ProjectDinosaur.MODID, "textures/gui/dino_scanner.png");
 
     public DinoScannerScreen(DinoScannerContainer dinoScannerContainer, Inventory inventory, Component title) {
-    super(dinoScannerContainer, inventory, title);
-
+        super(dinoScannerContainer, inventory, title);
+        this.titleLabelX = 80;
+        this.titleLabelY = 116;
     }
 
     @Override
@@ -36,8 +32,18 @@ public class DinoScannerScreen extends AbstractContainerScreen<DinoScannerContai
     @Override
     protected void renderBg(PoseStack pPoseStack, float pPartialTick, int pMouseX, int pMouseY) {
         RenderSystem.setShaderTexture(0, SCANNER);
-        int i = (this.width - 192) / 2;
-        int j = 2;
-        this.blit(pPoseStack, i, 2, 0, 0, 192, 192);
+        int i = this.getGuiLeft();
+        int j = this.getGuiTop();
+        this.blit(pPoseStack, i-40, j-20, 0, 0, 247, 169);
+    }
+
+    @Override
+    protected void renderLabels(PoseStack pPoseStack, int pMouseX, int pMouseY) {
+        this.font.draw(pPoseStack, this.title, (float)this.titleLabelX, (float)this.titleLabelY, 9544081);
+        this.font.draw(pPoseStack, new TranslatableComponent("screen.projectdinosaur.dino_age"), -24, 12, 9544081);
+        this.font.draw(pPoseStack, new TranslatableComponent("screen.projectdinosaur.dino_sex"), -24, 28, 9544081);
+        this.font.draw(pPoseStack, new TranslatableComponent("screen.projectdinosaur.dino_health"), -24, 44, 9544081);
+        this.font.draw(pPoseStack, new TranslatableComponent("screen.projectdinosaur.dino_food"), -24, 60, 9544081);
+        this.font.draw(pPoseStack, new TranslatableComponent("screen.projectdinosaur.dino_nocturnal"), -24, 76, 9544081);
     }
 }
--- src/main/java/com/kangalia/projectdinosaur/common/container/DinoScannerContainer.java
+++ src/main/java/com/kangalia/projectdinosaur/common/container/DinoScannerContainer.java
@@ -5,7 +5,6 @@ import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.MenuType;
-import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.Nullable;
 
 public class DinoScannerContainer extends AbstractContainerMenu {
