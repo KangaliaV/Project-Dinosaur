d785f1ce35f973839cafd9c9c415c9d0e2ecaf3e
mia <mia.bellclan@gmail.com>
2022-07-01 21:48:06 +0100

    Slowed the Compsognathus down.

--- src/main/java/com/kangalia/projectdinosaur/common/entity/creature/CompsognathusEntity.java
+++ src/main/java/com/kangalia/projectdinosaur/common/entity/creature/CompsognathusEntity.java
@@ -106,10 +106,10 @@ public class CompsognathusEntity extends PrehistoricEntity implements IAnimatabl
 
     protected void registerGoals() {
         this.goalSelector.addGoal(0, new FloatGoal(this));
-        this.goalSelector.addGoal(0, new PrehistoricBabyAvoidEntityGoal<>(this, Player.class, 4.0F, 1.5D, 1.5D));
-        this.goalSelector.addGoal(0, new PrehistoricBabyPanicGoal(this, 1.5D));
-        this.goalSelector.addGoal(1, new PrehistoricMeleeAttackGoal(this, 1.5D, true));
-        this.goalSelector.addGoal(2, new WaterAvoidingRandomStrollGoal(this, 2.0D, 200));
+        this.goalSelector.addGoal(0, new PrehistoricBabyAvoidEntityGoal<>(this, Player.class, 4.0F, 1.75D, 1.5D));
+        this.goalSelector.addGoal(0, new PrehistoricBabyPanicGoal(this, 1.75D));
+        this.goalSelector.addGoal(1, new PrehistoricMeleeAttackGoal(this, 1.75D, true));
+        this.goalSelector.addGoal(2, new WaterAvoidingRandomStrollGoal(this, 1.25D, 200));
         this.goalSelector.addGoal(3, new LookAtPlayerGoal(this, Player.class, 8.0F));
         this.goalSelector.addGoal(4, new RandomLookAroundGoal(this));
         this.targetSelector.addGoal(0, (new HurtByTargetGoal(this)).setAlertOthers());
