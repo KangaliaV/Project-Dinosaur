50dbcee27cb2f1a5bdebd49cb55b2a1c9e50686b
mia <mia.bellclan@gmail.com>
2022-09-19 22:31:57 +0100

    Added the recipe, texture and recipe book advancement for the Cryoporter

--- src/main/java/com/kangalia/projectdinosaur/core/init/ItemInit.java
+++ src/main/java/com/kangalia/projectdinosaur/core/init/ItemInit.java
@@ -101,6 +101,6 @@ public class ItemInit {
     public static final RegistryObject<Item> GROWTH_ACCELERATING_HORMONE = ITEMS.register("growth_accelerating_hormone", () -> new Item(new Item.Properties().stacksTo(64).tab(DinoItems.DINO_ITEMS)));
     public static final RegistryObject<Item> GROWTH_STUNTING_HORMONE = ITEMS.register("growth_stunting_hormone", () -> new Item(new Item.Properties().stacksTo(64).tab(DinoItems.DINO_ITEMS)));
     public static final RegistryObject<Item> DINO_SCANNER = ITEMS.register("dino_scanner", () -> new DinoScanner(new Item.Properties().stacksTo(1).tab(DinoItems.DINO_ITEMS)));
-    public static final RegistryObject<Item> CRYOPORTER = ITEMS.register("cryoporter", () -> new Cryoporter(new Item.Properties().stacksTo(1).durability(16).tab(DinoItems.DINO_ITEMS)));
+    public static final RegistryObject<Item> CRYOPORTER = ITEMS.register("cryoporter", () -> new Cryoporter(new Item.Properties().stacksTo(1).durability(10).tab(DinoItems.DINO_ITEMS)));
 
 }
